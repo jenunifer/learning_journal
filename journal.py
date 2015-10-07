321bace63425033b3b05fc651d31e0a2d1236678
Jen LaGrutta <jen@evergreenherbologist.com>
2015-10-07 15:03:55 -0700

    add journal changes

--- journal.py
+++ journal.py
@@ -5,21 +5,29 @@ from pyramid.config import Configurator
 from pyramid.view import view_config
 from waitress import serve
 import sqlalchemy as sa
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 import datetime
 
 Base = declarative_base()
 
-def Entry(Base):
+DATABASE_URL = os.environ.get (
+    'DATABASE_URL',
+    'postgresql://jens_mac:W1ck3ed1!@localhost:5432/learning-journal'
+)
+
+class Entry(Base):
     __tablename__ = 'entries'
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     title = sa.Column(sa.Unicode(127), nullable=False)
     text = sa.Column(sa.UnicodeText, nullable=False)
     created = sa.Column(
-	sa.DateTime, nullable=False, default=datetime.datetime.utcnow
+        sa.DateTime, nullable=False, default=datetime.datetime.utcnow
     )
-    	
 
+def init_db():
+    engine = sa.create_engine(DATABASE_URL)
+    Base.metadata.create_all(engine)
 
 @view_config(route_name='home', renderer='string')
 def home(request):
@@ -44,5 +52,6 @@ def main():
 
 if __name__ == '__main__':
     app = main()
+    
     port = os.environ.get('PORT', 5000)
     serve(app, host='0.0.0.0', port=port)
