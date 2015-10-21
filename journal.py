45cf94fb9114685cc5d406901f8320cd2041a5ec
Jen LaGrutta <jen@evergreenherbologist.com>
2015-10-21 10:48:48 -0700

    updated files, create test file

--- journal.py
+++ journal.py
@@ -6,8 +6,13 @@ from pyramid.view import view_config
 from waitress import serve
 import sqlalchemy as sa
 from sqlalchemy import create_engine
+from sqlalchemay import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import datetime
+from sqlalchemy.orm import scoped_session, sessionmaker
+from zope.sqlalchemy import ZopeTransactionExtension
+
+DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 
 Base = declarative_base()
 
@@ -40,10 +45,15 @@ def main():
     debug = os.environ.get('DEBUG', True)
     settings['reload_all'] = debug
     settings['debug_all'] = debug
+    if not os.environ.get('TESTING', False):
+        # only bind the session if we are not testing
+        engine = sa.create_engine(DATABASE_URL)
+        DBSession.configure(bind=engine)
     # configuration setup
     config = Configurator(
         settings=settings
     )
+    config.include('pyramid_tm')
     config.add_route('home', '/')
     config.scan()
     app = config.make_wsgi_app()
---
+++ test_journal.py
@@ -0,0 +1,36 @@
+from __future__ import unicode_literals
+import os
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.exc import IntegrityError
+
+TEST_DATABASE_URL = os.environ.get(
+    'DATABASE_URL',
+    'postgresql://jens_mac:@localhost:5432/test-learning-journal'
+)
+os.environ['DATABASE_URL'] = TEST_DATABASE_URL
+os.environ['TESTING'] = "True"
+@pytest.fixture(scope='session')
+def connection(request):
+    engine = create_engine(TEST_DATABASE_URL)
+    journal.Base.metadata.create_all(engine)
+    connection = engine.connect()
+    journal.DBSession.registry.clear()
+    journal.DBSession.configure(bind=connection)
+    journal.Base.metadata.bind = engine
+    request.addfinalizer(journal.Base.metadata.drop_all)
+    return connection
+
+@pytest.fixture()
+def db_session(request, connection):
+    from transaction import abort
+    trans = connection.begin()
+    request.addfinalizer(trans.rollback)
+    request.addfinalizer(abort)
+
+    from journal import DBSession
+    return DBSession
+
+
+
+import journal
